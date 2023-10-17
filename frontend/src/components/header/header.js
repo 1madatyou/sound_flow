99c6e813864b0e0392e3c8242b5c2bb880534576
1madatyou <khasanov.amir1323@gmail.com>
2023-10-17 16:23:52 +0700

    Implement header, logo react components

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,13 @@
+import Logo from "../logo/logo";
+import "./header.css"
+
+
+function Header() {
+    return (
+        <header>
+            <Logo/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ frontend/src/components/logo/logo.js
@@ -0,0 +1,11 @@
+import "./logo.css"
+
+function Logo() {
+    return (
+        <div className="logo">
+            SoundFlow
+        </div>       
+    );
+}
+
+export default Logo;
