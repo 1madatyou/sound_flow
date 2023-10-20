375416afd64345ac7dd3b5798d77cb8aef325d47
1madatyou <khasanov.amir1323@gmail.com>
2023-10-20 17:17:27 +0700

    Realize UserUpdate model which represents user actions

--- backend/sound_flow/statistics/models.py
+++ backend/sound_flow/statistics/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class UserStatistic(models.Model):
@@ -9,3 +10,18 @@ class UserStatistic(models.Model):
     count_of_playlists = models.IntegerField(default=0)
     count_of_streams = models.IntegerField(default=0)
     count_of_followers = models.IntegerField(default=0)
+
+class UserUpdate(models.Model):
+    ''' Represents user actions such as downloading music, creating playlist, etc. '''
+
+    REASON_CHOICES = [
+        ('TRACK_ADD', 'Track added'),
+        ('TRACK_UPLOAD', 'Track uploaded'),
+        ('PLAYLIST_CREATE', 'Playlist created'),
+        ('FOLLOWING_START', 'Following started')
+    ]
+
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    reason = models.CharField(verbose_name='Reason of update, for example downloading a track', choices=REASON_CHOICES)
+
+    creation_date = models.DateTimeField(default=timezone.now)
